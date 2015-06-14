a7316e826eb95664095da16ee9b2276348734853
=Kipyung Sung <sungkipyung@gmail.com>
2015-06-14 13:31:49 +0900

    PHFramework example commit

--- SampleCodeForImageHandling/ViewController.m
+++ SampleCodeForImageHandling/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import <Photos/Photos.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -17,6 +19,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    PHFetchOptions *fetchOptions = [PHFetchOptions new];
+    fetchOptions.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO]];
+    fetchOptions.includeHiddenAssets = NO;
+    fetchOptions.predicate = [NSPredicate predicateWithFormat:@"(mediaSubtype & %d) != 0 || (mediaSubtype & %d) != 0", PHAssetMediaSubtypePhotoPanorama, PHAssetMediaSubtypeVideoHighFrameRate];
+    
+    PHFetchResult *assetsInfo = [PHAsset fetchAssetsWithOptions:fetchOptions];
+    
+    PHImageRequestOptions *requestOptions = [PHImageRequestOptions new];
+    requestOptions.version = PHImageRequestOptionsVersionCurrent;
+//    requestOptions.deliveryMode = PHImageRequestOptionsDeliveryModeFastFormat;
+    requestOptions.deliveryMode = PHImageRequestOptionsDeliveryModeHighQualityFormat;
+    requestOptions.resizeMode = PHImageRequestOptionsResizeModeExact;
+    
+    [assetsInfo enumerateObjectsUsingBlock:^(PHAsset *obj, NSUInteger idx, BOOL *stop) {
+        NSLog(@"Asset Info : %@",
+              [@{@"localIdentifier" :obj.localIdentifier,
+                 @"mediaType":@(obj.mediaType)
+                 ,@"mediaSubtype":@(obj.mediaSubtypes)
+                 ,@"pixelWidth":@(obj.pixelWidth)
+                 ,@"pixelHeight":@(obj.pixelHeight)
+                 } description]);
+        
+        CGFloat suggestedImageWidth = MIN(1024, obj.pixelWidth);
+        CGFloat suggestedImageHeight = obj.pixelHeight * suggestedImageWidth / obj.pixelWidth;
+        
+        [[PHImageManager defaultManager] requestImageForAsset:obj
+                                                   targetSize:CGSizeMake(suggestedImageWidth, suggestedImageHeight)
+                                                  contentMode:PHImageContentModeDefault
+                                                      options:requestOptions
+                                                resultHandler:^(UIImage *result, NSDictionary *info) {
+                                                    if (result) {
+                                                        // galleryButton is just a UIButton in the view
+                                                        self.imageView.image = result;
+                                                        NSLog(@"image : %@", result);
+                                                        NSLog(@"info : %@", info);
+                                                        //                                                    [galleryButton setImage:result forState:UIControlStateNormal];
+                                                    }
+                                                }];
+
+    }];
+    // obj.pixelWidth : obj.pixelHeight =  suggestedImageWidth : x
+    // suggestedImageHeight = pixelHeight * suggestedImageWidth / obj.pixelWidth
 }
 
 - (void)didReceiveMemoryWarning {
